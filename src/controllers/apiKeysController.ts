c97c86988a87c59cfbf887eb84ed288b782c01fb
William Fish <william.michael.fish@gmail.com>
2024-03-27 11:23:09 +0000

    FPO-147: Code review

--- src/controllers/apiKeysController.ts
+++ src/controllers/apiKeysController.ts
@@ -1,35 +1,34 @@
 import { type Request, type Response } from 'express'
-import { CustomerApiKeyRepository } from '../repositories/customerApiKeyRepository'
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { APIGatewayClient } from '@aws-sdk/client-api-gateway'
-
-const dynamodbClient = new DynamoDBClient({ region: process.env.AWS_REGION })
-const apiGatewayClient = new APIGatewayClient({ region: process.env.AWS_REGION })
-const repository = new CustomerApiKeyRepository(dynamodbClient, apiGatewayClient)
-const allowedKeys = ['description', 'enabled']
+import { type CustomerApiKeyRepository } from '../repositories/customerApiKeyRepository'
+// const allowedKeys = ['description', 'enabled']
 
 export class ApiKeyController {
-//   async update (req: Request, res: Response): Promise<void> {
-//     const fpoId = req.params.fpoId
-//     const id = req.params.id
-//     const body = req.body
+  //   async update (req: Request, res: Response): Promise<void> {
+  //     const fpoId = req.params.fpoId
+  //     const id = req.params.id
+  //     const body = req.body
+  //
+  //     if (typeof body !== 'object') {
+  //       res.status(400).json({ message: 'Invalid request' })
+  //     }
+  //     const apiKey = await repository.updateKey(fpoId, id, body)
 
-//     if (typeof body !== 'object') {
-//       res.status(400).json({ message: 'Invalid request' })
-//     }
-//     const apiKey = await repository.updateKey(fpoId, id, body)
+  //     if (apiKey === null) {
+  //       res.status(404).json({ message: 'Did not succeed' })
+  //     } else {
+  //       res.status(200).json(apiKey.toJson())
+  //     }
+  //   }
+  private readonly repository: CustomerApiKeyRepository
 
-//     if (apiKey === null) {
-//       res.status(404).json({ message: 'Did not succeed' })
-//     } else {
-//       res.status(200).json(apiKey.toJson())
-//     }
-//   }
+  constructor (repository: CustomerApiKeyRepository) {
+    this.repository = repository
+  }
 
   async show (req: Request, res: Response): Promise<void> {
     const fpoId = req.params.fpoId
     const id = req.params.id
-    const apiKey = await repository.getKey(fpoId, id)
+    const apiKey = await this.repository.getKey(fpoId, id)
 
     if (apiKey === null) {
       res.status(404).json({ message: 'API key not found' })
@@ -40,14 +39,14 @@ export class ApiKeyController {
 
   async index (req: Request, res: Response): Promise<void> {
     const fpoId = req.params.fpoId
-    const apiKeys = await repository.listKeys(fpoId)
+    const apiKeys = await this.repository.listKeys(fpoId)
 
     res.json(apiKeys.map(apiKey => apiKey.toJson()))
   }
 
   async create (req: Request, res: Response): Promise<void> {
     const fpoId = req.params.fpoId
-    const apiKey = await repository.createKey(fpoId)
+    const apiKey = await this.repository.createKey(fpoId)
 
     res.status(201).json(apiKey.toJson())
   }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,15 +2,25 @@ import express, { type Router } from 'express'
 import { HealthchecksController } from '../controllers/healthchecksController'
 import { ApiKeyController } from '../controllers/apiKeysController'
 
-const apiKeyController = new ApiKeyController()
+import { CustomerApiKeyRepository } from '../repositories/customerApiKeyRepository'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { APIGatewayClient } from '@aws-sdk/client-api-gateway'
+
+const dynamodbClient = new DynamoDBClient({ region: process.env.AWS_REGION })
+const apiGatewayClient = new APIGatewayClient({ region: process.env.AWS_REGION })
+const repository = new CustomerApiKeyRepository(dynamodbClient, apiGatewayClient)
+
+const apiKeyController = new ApiKeyController(repository)
 const healthchecksController = new HealthchecksController()
 
 const router: Router = express.Router()
 
-router.get('/healthcheck', healthchecksController.show)
-router.get('/healthcheckz', healthchecksController.showz)
-router.post('/keys/:fpoId', apiKeyController.create)
-router.get('/keys/:fpoId', apiKeyController.index)
-router.get('/keys/:fpoId/:id', apiKeyController.show)
+router.get('/healthcheck', (req, res) => { healthchecksController.show(req, res) })
+router.get('/healthcheckz', (req, res) => { healthchecksController.showz(req, res) })
+
+// ESlint does not understand async express route handlers
+router.post('/keys/:fpoId', (req, res) => { apiKeyController.create(req, res) }) // eslint-disable-line @typescript-eslint/no-floating-promises
+router.get('/keys/:fpoId', (req, res) => { apiKeyController.index(req, res) }) // eslint-disable-line @typescript-eslint/no-floating-promises
+router.get('/keys/:fpoId/:id', (req, res) => { apiKeyController.show(req, res) }) // eslint-disable-line @typescript-eslint/no-floating-promises
 
 export default router
