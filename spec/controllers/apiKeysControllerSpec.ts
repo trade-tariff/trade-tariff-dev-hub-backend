08371127ae9fbc9d964cd52e4f9a2d123c4a7e73
William Fish <william.michael.fish@gmail.com>
2024-04-02 15:26:58 +0100

    FPO-149: Code review

--- spec/controllers/apiKeysControllerSpec.ts
+++ spec/controllers/apiKeysControllerSpec.ts
@@ -95,6 +95,52 @@ describe('ApiKeyController', () => {
     })
   })
 
+  describe('update', () => {
+    const res = {
+      status: function (code: number) {
+        this.statusCode = code
+        return this
+      },
+      json: function (data: any) {
+        this.data = data
+        return this
+      }
+    } as any
+
+    it('updates the key', async () => {
+      apiKey = new CustomerApiKey()
+      const getKeyResult = Promise.resolve(apiKey)
+      const updateKeyResult = Promise.resolve(apiKey)
+
+      repository = jasmine.createSpyObj(
+        'CustomerApiKeyRepository',
+        {
+          updateKey: updateKeyResult,
+          getKey: getKeyResult
+        }
+      )
+      repository.updateKey.bind(repository)
+      controller = new ApiKeyController(repository)
+      req = { params: { fpoId: 'fpoId', id: 'id' }, body: { description: 'description' } } as any
+
+      await controller.update(req, res)
+
+      expect(repository.getKey).toHaveBeenCalledWith('fpoId', 'id')
+      expect(repository.updateKey).toHaveBeenCalledWith(apiKey)
+      expect(res.statusCode).toBe(200)
+      expect(res.data).toEqual({
+        CustomerApiKeyId: '',
+        ApiGatewayId: '',
+        Secret: '',
+        Enabled: false,
+        Description: 'description',
+        FpoId: '',
+        CreatedAt: apiKey.CreatedAt,
+        UpdatedAt: apiKey.UpdatedAt
+      })
+    })
+  })
+
   describe('create', () => {
     it('creates a new key', async () => {
       apiKey = new CustomerApiKey()
