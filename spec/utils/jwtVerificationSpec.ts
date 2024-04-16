f4068cda3cd2d2beb0e410d0fd26107a68b81598
Amber Cronin <amber@amber.vision>
2024-04-16 14:48:02 +0100

    FPO-154: Fix linting issues

--- spec/utils/jwtVerificationSpec.ts
+++ spec/utils/jwtVerificationSpec.ts
@@ -1,5 +1,5 @@
 import 'jasmine'
-import { type NextFunction, type Request, type Response } from 'express'
+import { type NextFunction } from 'express'
 
 import { verifyToken } from '../../src/utils/jwtVerify'
 
@@ -7,37 +7,39 @@ describe('JWT verification', () => {
   const next: NextFunction = jasmine.createSpy()
 
   describe('on unprotected paths', () => {
-    let req = {
+    const req = {
       headers: {
         authorization: ''
       },
       path: '/api/healthcheck'
     } as any
-    let res = {
+    const res = {
       status: jasmine.createSpy().and.returnValue({ json: jasmine.createSpy() })
     } as unknown as any
 
     it('is skipped', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       verifyToken(req, res, next)
       expect(next).toHaveBeenCalled()
     })
   })
 
   describe('on protected paths', () => {
-    let req = {
+    const req = {
       headers: {
         authorization: ''
       },
       path: '/api/keys/1'
     } as any
 
-    let res = {
+    const res = {
       status: jasmine.createSpy().and.returnValue({ json: jasmine.createSpy() })
     } as any
 
     const next: NextFunction = jasmine.createSpy()
 
     it('responds with 401 if Authorization header is missing', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       verifyToken(req, res, next)
       expect(res.status).toHaveBeenCalledWith(401)
       expect(next).not.toHaveBeenCalled()
