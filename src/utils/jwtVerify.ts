b6facd7a444075d9b0dc3d16e3c2f30d0596eb55
William Fish <william.michael.fish@gmail.com>
2024-05-09 15:10:14 +0100

    BAU: Remove debug lines

--- src/utils/jwtVerify.ts
+++ src/utils/jwtVerify.ts
@@ -11,11 +11,8 @@ const excludedPaths = [
 
 const getKey = async (tokenHeader: JwtHeader): Promise<string | undefined> => {
   const client = jwksClient({ jwksUri })
-  logger.info(`Token Header: ${JSON.stringify(tokenHeader)}`)
   const key = await client.getSigningKey(tokenHeader.kid)
-  logger.info(`Key from JWKS: ${JSON.stringify(key)}`)
   const publicKey = key.getPublicKey()
-  logger.info(`Public Key: ${publicKey}`)
   return publicKey
 }
 
@@ -28,25 +25,19 @@ export const verifyToken: RequestHandler = (req: Request, res: Response, next: N
   let authorised: boolean = false
 
   if (bearer !== undefined) {
-    logger.info(`Authorisation: ${bearer}`)
     // Authorisation: Bearer <token>
     const token = bearer.split(' ')[1]
-    logger.info(`Token: ${token}`)
     const decodedToken = decode(token, { complete: true })
-    logger.info(`Decoded Token: ${JSON.stringify(decodedToken)}`)
 
     if (decodedToken !== null) {
       if (decodedToken.header.kid !== null) {
         getKey(decodedToken.header)
           .then(key => {
             if (key === undefined) {
-              logger.info('Key is undefined')
+              logger.error('Key is undefined')
               authorised = false
             } else {
-              verify(token, key, (err, payload) => {
-                logger.info(`Payload: ${JSON.stringify(payload)}`)
-                logger.info(`Error: ${JSON.stringify(err)}`)
-
+              verify(token, key, (_err, payload) => {
                 if (payload !== undefined) {
                   logger.info('*****************************************Authorised*****************************************')
                   authorised = true
@@ -56,14 +47,14 @@ export const verifyToken: RequestHandler = (req: Request, res: Response, next: N
             }
           })
           .catch((error) => {
-            logger.info(`Caught Error: ${error}`)
+            logger.error(`Caught Error: ${error}`)
             authorised = false
           })
       }
     }
   }
   if (!authorised) {
-    logger.info('Unauthorised')
+    logger.error('Unauthorised')
     res.status(401).json({
       message: 'Unauthorised'
     })
